1ea60b25b74fcf5d523676f510f7cfbb548c6d70
0xZephyR <1937176774@qq.com>
2021-12-18 21:13:52 +0800

    nothing

--- src/components/IndexPage.tsx
+++ src/components/IndexPage.tsx
@@ -1,9 +1,6 @@
-import { Carousel } from 'antd';
+import { Avatar, Carousel, Image, List } from 'antd';
 import React from 'react';
 import '../css/carousel-custom.scss';
-import { Avatar, Image, List } from 'antd';
-import { UserOutlined } from '@ant-design/icons';
-import { SubMenu } from 'rc-menu';
 const contentStyle: React.CSSProperties = {
 	color: '#fff',
 	lineHeight: '160px',
@@ -17,76 +14,88 @@ const contentStyle: React.CSSProperties = {
 
 const CustomCarousel = () => {
 	return (
-    <div className="wrapper">
-      <Carousel
-        arrows
-        autoplay
-        autoplaySpeed={2000}
-        dotPosition={'bottom'}
-        dots
-      >
-        <div>
-          <h3 style={contentStyle}>Math</h3>
-        </div>
-        <div>
-          <h3 style={contentStyle}>2</h3>
-        </div>
-        <div>
-          <h3 style={contentStyle}>3</h3>
-        </div>
-        <div>
-          <h3 style={contentStyle}>4</h3>
-        </div>
-      </Carousel>
-    </div>
-  );
+		<div className="wrapper">
+			<Carousel
+				arrows
+				autoplay
+				autoplaySpeed={2000}
+				dotPosition={'bottom'}
+				dots
+			>
+				<div>
+					<h3 style={contentStyle}>Math</h3>
+				</div>
+				<div>
+					<h3 style={contentStyle}>2</h3>
+				</div>
+				<div>
+					<h3 style={contentStyle}>3</h3>
+				</div>
+				<div>
+					<h3 style={contentStyle}>4</h3>
+				</div>
+			</Carousel>
+		</div>
+	);
 };
 
-const Avater=()=>{
-	return <div>
-    <Avatar src={<Image src="https://joeschmoe.io/api/v1/random" style={{ width: 32}} />}/>
-	</div>
-}
-
-const Info=()=>{
-	const data = [
-    {
-      title: 'Ant Design Title 1'
-    },
-    {
-      title: 'Ant Design Title 2'
-    },
-    {
-      title: 'Ant Design Title 3'
-    },
-    {
-      title: 'Ant Design Title 4'
-    }
-  ];
+const Avater = () => {
 	return (
-    <div>
-      <List
-        dataSource={data}
-        itemLayout="horizontal"
-        renderItem={(item) => (
-          <List.Item>
-            <List.Item.Meta
-              avatar={<Avatar src="https://joeschmoe.io/api/v1/random" />}
-              description="Ant Design, a design language for background applications, is refined by Ant UED Team"
-              title={<a href="https://ant.design">{item.title}</a>}
-            />
-          </List.Item>
-        )}
-      />
-    </div>
-  );
-}
+		<div>
+			<Avatar
+				src={
+					<Image
+						src="https://joeschmoe.io/api/v1/random"
+						style={{ width: 32 }}
+					/>
+				}
+			/>
+		</div>
+	);
+};
+
+const Info = () => {
+	const data = [
+		{
+			title: 'Ant Design Title 1'
+		},
+		{
+			title: 'Ant Design Title 2'
+		},
+		{
+			title: 'Ant Design Title 3'
+		},
+		{
+			title: 'Ant Design Title 4'
+		}
+	];
+	return (
+		<div>
+			<List
+				dataSource={data}
+				itemLayout="horizontal"
+				renderItem={(item) => (
+					<List.Item>
+						<List.Item.Meta
+							avatar={
+								<Avatar src="https://joeschmoe.io/api/v1/random" />
+							}
+							description="Ant Design, a design language for background applications, is refined by Ant UED Team"
+							title={
+								<a href="https://ant.design">{item.title}</a>
+							}
+						/>
+					</List.Item>
+				)}
+			/>
+		</div>
+	);
+};
 
 export const IndexPage = () => {
 	return (
-    <>
-     
-	  <Info />
-    </>
-  );
+		<>
+			<Info />
+		</>
+	);
 };
--- webpack.config.js
+++ webpack.config.js
@@ -15,16 +15,6 @@ export default {
 	module: {
 		// 所有第三方模块的匹配规则， webpack默认只能处理.js后缀名的文件，需要安装第三方loader
 		rules: [
-			// {
-			// 	test: /\.(js|jsx)$/,
-			// 	use: {
-			// 		loader: 'babel-loader',
-			// 		options: {
-			// 			presets: ['@babel/preset-env']
-			// 		}
-			// 	},
-			// 	exclude: /(node_modules|bower_components)/ // 千万别忘记添加exclude选项,不然运行可能会报错
-			// },
 			{
 				test: /\.tsx?$/,
 				use: 'ts-loader',
